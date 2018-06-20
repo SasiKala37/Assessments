d030c6cfc6e0459a68de02df45eb51d9a7e7b0e6
bridgelabz <SasiKala37>
2018-06-20 09:26:10 +0530

    jdbc Programs

--- src/com/bridgelabz/sample/BatchOfUser.java
+++ src/com/bridgelabz/sample/BatchOfUser.java
@@ -1,15 +1,42 @@
 package com.bridgelabz.sample;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BatchOfUser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 	Connection connection=null;
 	Statement statement=null;
-	String 
+	String insert="insert into studentdetails values(4,'prateek')";
+	String update="update studentdetails set id=6 where name='simran'";
+	String delete="delete from studentdetails where id=2";
+	try {
+	connection=Utility.connections();
+	statement=connection.createStatement();
+	
+	statement.addBatch(insert);
+	statement.addBatch(update);
+	statement.addBatch(delete);
 
+	int[] array=statement.executeBatch();
+	for (int i:array) {
+		System.out.println(i);
+	}
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}
+	
+	finally {
+		if(statement!=null) {
+			statement.close();
+		}
+		if(connection!=null) {
+			connection.close();
+		}
+	}
+	
 	}
 
 }
